4bf90fa1fb2af9f4b0b149f43e7dc4c77145ee05
thashreef <chthashreef22@gmail.com>
2024-06-29 14:31:20 +0400

    Skeleton Changes

--- client/src/components/Skeleton/Skeleton.jsx
+++ client/src/components/Skeleton/Skeleton.jsx
@@ -1,14 +1,16 @@
 
+import { useContext } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { modeContext } from '../../context/DarkMode';
 import './skeleton.css';
 
 function SkeletonLoading({type}) {
 
-    
+    const {darkMode} = useContext(modeContext)
     return (
         <div className={type+'Main'}>
-            <Skeleton  className={type}  highlightColor="#0000006a" baseColor="gray"/>
+            <Skeleton  className={type}  highlightColor={darkMode?"#0000006a":''} baseColor={darkMode? "gray":''}/>
         </div>
     );
 }
